3a4ad573465d2a4c7be0398c8e1ad2b10257cb66
ceoDemitri <demichole@gmail.com>
2024-12-22 00:50:35 -0600

    Day One; Natural intelligence. Lays out building blocks with simple home with email sign form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- :by Cristina Gottardi: https://images.unsplash.com/profile-1509140709644-5b9d6cd408f9?w=32&dpr=2&crop=faces&bg=%23fff&h=32&auto=format&fit=crop&q=60&ixlib=rb-4.0.3 -->
+<img
+	src="https://images.unsplash.com/photo-1522787345986-d5c7885a889e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+	alt=""
+/>
+
+<article class="text-center">
+	<h1>Let's Build a <span>Brighter</span> Future</h1>
+	<p class="subtitle">Testing Font</p>
+	<p class="text m-auto w-80 text-justify text-sm">
+		Together we may find lighter roads to travel. The future is ready to be built and we have every
+		tool we need at our disposal. This is a place where you will realize your worth finding more
+		reasons to explore this realm. Thank you.
+	</p>
+</article>
+
+<form action="" method="post" class="text-center">
+	<label for="email">Stay Connected</label>
+	<input id="email" type="email" name="email" class="border border-blue-600" />
+	<input type="submit" value="submit" />
+</form>
+
+<style>
+	* {
+		color: midnightblue;
+		font-family: 'reross-quadratic', sans-serif;
+		font-weight: 400;
+		font-style: normal;
+	}
+
+	h1 span {
+		font-family: 'playwrite-cc-hr-lijeva', sans-serif;
+		font-weight: 400;
+		font-style: normal;
+	}
+
+	.subtitle {
+		font-family: 'playwrite-cc-hr-lijeva', sans-serif;
+		font-weight: 200;
+		font-style: normal;
+		font-size: 0.9em;
+	}
+</style>
